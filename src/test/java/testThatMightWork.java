9501f821200b24c1fc71c2add21ecfe1abb7e08d
Takumi <Example@template.com>
2024-11-21 17:20:02 +0000

    Added another test.

---
+++ src/test/java/testThatMightWork.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testThatMightWork
+{
+    @Test
+    void integrationTest2()
+    {
+        assertEquals(5, 5);
+    }
+}
